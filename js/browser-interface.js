dc7d78790322579533685a073aeccfd6fac45ba9
James Nielson <james@changemachine.org>
2016-02-19 17:03:37 -0800

    Search GH users & return user repos.

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,12 +1,20 @@
 var dataLoad = require('./../js/dataLoad-interface.js').dataLoad;
 
 $(function() {
-  $.get({
-    url: 'https://api.github.com/users/changemachine/repos',
-    dataType: 'json',
-    userAgent: 'test',
-    success: function(data) {
-      dataLoad(data);
-    }// /success
+  $("#userName").submit(function(e){
+    $.get({
+      url: 'https://api.github.com/users/changemachine/repos',
+      dataType: 'json',
+      userAgent: 'test',
+      success: function(data) {
+        $('#userName').html("<h2>"+ data[0].owner.login +"</h2>").append("<h4>Repositories:</h4>");
+        // console.log(data[0].owner.login);
+        dataLoad(data);
+      }// /success
+
   });// /get
+  e.preventDefault();
+});
+
+
 }); // /doc-ready
